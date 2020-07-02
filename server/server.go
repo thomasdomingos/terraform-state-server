9d58e857f96dbdd340945348633e802f6011d358
Thomas Domingos <thomas.domingos@gmail.com>
2020-07-03 00:01:58 +0200

    Add the possibility to name state and to list all states present inside directory

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/thomasdomingos/terraform-state-server/config"
@@ -19,28 +20,59 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to Terraform HTTP State Server!")
 }
 
-func assertFile(name string) error {
-	_, err := os.Stat(name)
-	if err == nil {
-		return nil
+func assertState(stateName string) error {
+	// Test directory containing state
+	if _, err := os.Stat(filepath.Join(registryPath, stateName)); err != nil {
+		// Create directory that will contain state file(s)
+		if os.IsNotExist(err) {
+			log.Println("Directory does not not exist, creating it")
+			if err := os.MkdirAll(filepath.Join(registryPath, stateName), os.ModePerm); err != nil {
+				return err
+			}
+		}
 	}
-	if os.IsNotExist(err) {
-		log.Println("File does not exist, creating it")
-		file, err := os.OpenFile(name, os.O_RDONLY|os.O_CREATE, 0644)
-		defer file.Close()
-		if err != nil {
-			return err
+	// Create an empty file when state does not exists
+	if _, err := os.Stat(filepath.Join(registryPath, stateName, "state")); err != nil {
+		if os.IsNotExist(err) {
+			log.Println("Creating empty state file")
+			file, err := os.OpenFile(filepath.Join(registryPath, stateName, "state"), os.O_RDONLY|os.O_CREATE, 0644)
+			defer file.Close()
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
-func getState(w http.ResponseWriter, r *http.Request) {
-	log.Println("HIT: getState")
-	if err := assertFile(filepath.Join(registryPath, "state")); err != nil {
+func getStates(w http.ResponseWriter, r *http.Request) {
+	log.Println("HIT: getStates")
+
+	states, err := ioutil.ReadDir(registryPath)
+	if err != nil {
 		log.Fatal(err)
 	}
-	file, err := os.Open(filepath.Join(registryPath, "state"))
+	type State struct {
+		Name string `json:"Name"`
+	}
+	States := make([]State, 0)
+	for _, f := range states {
+		if f.IsDir() {
+			States = append(States, State{f.Name()})
+		}
+	}
+	json.NewEncoder(w).Encode(States)
+}
+
+func getState(w http.ResponseWriter, r *http.Request) {
+	log.Println("HIT: getState")
+	vars := mux.Vars(r)
+	stateName := vars["id"]
+	if err := assertState(stateName); err != nil {
+		log.Fatal(err)
+	}
+	// Read state file and return its content
+	file, err := os.Open(filepath.Join(registryPath, stateName, "state"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,13 +89,15 @@ func getState(w http.ResponseWriter, r *http.Request) {
 
 func postState(w http.ResponseWriter, r *http.Request) {
 	log.Println("HIT: postState")
-	if err := assertFile(filepath.Join(registryPath, "state")); err != nil {
+	vars := mux.Vars(r)
+	stateName := vars["id"]
+	if err := assertState(stateName); err != nil {
 		log.Fatal(err)
 	}
 
+	// Read content of the POST data, and write it to the corresponding state file
 	reqBody, err := ioutil.ReadAll(r.Body)
-
-	file, err := os.OpenFile(filepath.Join(registryPath, "state"), os.O_WRONLY, 0644)
+	file, err := os.OpenFile(filepath.Join(registryPath, stateName, "state"), os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -80,8 +114,9 @@ func Serve(cfg config.Config) error {
 	log.Println("Serving Terraform HTTP State Server")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homepage)
-	router.HandleFunc("/state", getState).Methods("GET")
-	router.HandleFunc("/state", postState).Methods("POST")
+	router.HandleFunc("/states", getStates).Methods("GET")
+	router.HandleFunc("/state/{id}", getState).Methods("GET")
+	router.HandleFunc("/state/{id}", postState).Methods("POST")
 	registryPath = cfg.Registry.Path
 	return http.ListenAndServe(fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port), router)
 }
